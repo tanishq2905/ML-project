16f29a470163b05c5ed55fe8aff8525f6333c5f3
tanishq2905 <tkota@gitam.in>
2026-03-25 10:25:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ y = data['mood']
 st.write(y.value_counts())
 # Split
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42, stratify=y
+    X, y, test_size=0.2, random_state=42
 )
 
 # Visualization
